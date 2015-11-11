3cfa07be99bf13df8a624a04645cad01e32fd477
Giacomo Tanganelli <giacomo.tanganelli@for.unipi.it>
2015-11-11 15:51:23 +0100

    ex-lab4

---
+++ lab4/01-RPLDIO/obj_z1/node-id.d
@@ -0,0 +1,12 @@
+obj_z1/node-id.o: /home/user/contiki/platform/z1/./node-id.c \
+ /home/user/contiki/core/./sys/node-id.h \
+ /home/user/contiki/platform/z1/./contiki-conf.h \
+ /home/user/contiki/platform/z1/./platform-conf.h \
+ /home/user/contiki/cpu/msp430/./msp430def.h \
+ /home/user/contiki/core/./dev/xmem.h
+/home/user/contiki/platform/z1/./node-id.c :
+ /home/user/contiki/core/./sys/node-id.h :
+ /home/user/contiki/platform/z1/./contiki-conf.h :
+ /home/user/contiki/platform/z1/./platform-conf.h :
+ /home/user/contiki/cpu/msp430/./msp430def.h :
+ /home/user/contiki/core/./dev/xmem.h :
---
+++ lab4/01-RPLDIO/obj_z1/trickle-timer.d
@@ -0,0 +1,28 @@
+obj_z1/trickle-timer.o: /home/user/contiki/core/lib/trickle-timer.c \
+ /home/user/contiki/platform/z1/./contiki-conf.h \
+ /home/user/contiki/platform/z1/./platform-conf.h \
+ /home/user/contiki/cpu/msp430/./msp430def.h \
+ /home/user/contiki/core/./lib/trickle-timer.h \
+ /home/user/contiki/core/./sys/ctimer.h \
+ /home/user/contiki/core/./sys/etimer.h \
+ /home/user/contiki/core/./sys/timer.h \
+ /home/user/contiki/core/./sys/clock.h \
+ /home/user/contiki/core/./sys/process.h \
+ /home/user/contiki/core/./sys/pt.h /home/user/contiki/core/./sys/lc.h \
+ /home/user/contiki/core/./sys/lc-switch.h \
+ /home/user/contiki/core/./sys/cc.h \
+ /home/user/contiki/core/./lib/random.h
+/home/user/contiki/core/lib/trickle-timer.c :
+ /home/user/contiki/platform/z1/./contiki-conf.h :
+ /home/user/contiki/platform/z1/./platform-conf.h :
+ /home/user/contiki/cpu/msp430/./msp430def.h :
+ /home/user/contiki/core/./lib/trickle-timer.h :
+ /home/user/contiki/core/./sys/ctimer.h :
+ /home/user/contiki/core/./sys/etimer.h :
+ /home/user/contiki/core/./sys/timer.h :
+ /home/user/contiki/core/./sys/clock.h :
+ /home/user/contiki/core/./sys/process.h :
+ /home/user/contiki/core/./sys/pt.h /home/user/contiki/core/./sys/lc.h :
+ /home/user/contiki/core/./sys/lc-switch.h :
+ /home/user/contiki/core/./sys/cc.h :
+ /home/user/contiki/core/./lib/random.h :
