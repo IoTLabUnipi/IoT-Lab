396dfe1f5fa5109ed2413c8ac897275059b4f5c9
Tanganelli <giacomo.tanganelli@for.unipi.it>
2015-11-19 10:20:57 +0100

    LAB5

---
+++ lab5/02-GET-POST/obj_z1/gcr.d
@@ -0,0 +1,2 @@
+obj_z1/gcr.o: /home/user/contiki/core/lib/gcr.c
+/home/user/contiki/core/lib/gcr.c :
---
+++ lab5/03-QUERY/obj_z1/assert.d
@@ -0,0 +1,2 @@
+obj_z1/assert.o: /home/user/contiki/core/lib/assert.c
+/home/user/contiki/core/lib/assert.c :
