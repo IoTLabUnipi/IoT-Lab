c6eb7dc5a75027f1a78f5e082ffe35ba727ab2d4
Giacomo Tanganelli <giacomo.tanganelli@for.unipi.it>
2015-11-26 15:10:11 +0100

    LAB6_EX

---
+++ lab6/03-ETAG/obj_z1/radio-sensor.d
@@ -0,0 +1,68 @@
+obj_z1/radio-sensor.o: /home/user/contiki/platform/z1/dev/radio-sensor.c \
+ /home/user/contiki/core/./lib/sensors.h \
+ /home/user/contiki/core/./contiki.h \
+ /home/user/contiki/core/./contiki-version.h \
+ /home/user/contiki/platform/z1/./contiki-conf.h \
+ /home/user/contiki/platform/z1/./platform-conf.h \
+ /home/user/contiki/cpu/msp430/./msp430def.h project-conf.h \
+ /home/user/contiki/core/./contiki-default-conf.h \
+ /home/user/contiki/core/./sys/process.h \
+ /home/user/contiki/core/./sys/pt.h /home/user/contiki/core/./sys/lc.h \
+ /home/user/contiki/core/./sys/lc-switch.h \
+ /home/user/contiki/core/./sys/cc.h \
+ /home/user/contiki/core/./sys/autostart.h \
+ /home/user/contiki/core/./sys/process.h \
+ /home/user/contiki/core/./sys/timer.h \
+ /home/user/contiki/core/./sys/clock.h \
+ /home/user/contiki/core/./sys/ctimer.h \
+ /home/user/contiki/core/./sys/etimer.h \
+ /home/user/contiki/core/./sys/timer.h \
+ /home/user/contiki/core/./sys/etimer.h \
+ /home/user/contiki/core/./sys/rtimer.h \
+ /home/user/contiki/cpu/msp430/./rtimer-arch.h \
+ /home/user/contiki/core/./sys/rtimer.h \
+ /home/user/contiki/core/./sys/pt.h \
+ /home/user/contiki/core/./sys/procinit.h \
+ /home/user/contiki/core/./sys/loader.h \
+ /home/user/contiki/core/./sys/clock.h \
+ /home/user/contiki/core/./sys/energest.h \
+ /home/user/contiki/dev/cc2420/cc2420.h \
+ /home/user/contiki/core/./dev/spi.h \
+ /home/user/contiki/core/./dev/radio.h \
+ /home/user/contiki/dev/cc2420/cc2420_const.h \
+ /home/user/contiki/core/./lib/aes-128.h \
+ /home/user/contiki/core/./dev/radio-sensor.h
+/home/user/contiki/platform/z1/dev/radio-sensor.c :
+ /home/user/contiki/core/./lib/sensors.h :
+ /home/user/contiki/core/./contiki.h :
+ /home/user/contiki/core/./contiki-version.h :
+ /home/user/contiki/platform/z1/./contiki-conf.h :
+ /home/user/contiki/platform/z1/./platform-conf.h :
+ /home/user/contiki/cpu/msp430/./msp430def.h project-conf.h :
+ /home/user/contiki/core/./contiki-default-conf.h :
+ /home/user/contiki/core/./sys/process.h :
+ /home/user/contiki/core/./sys/pt.h /home/user/contiki/core/./sys/lc.h :
+ /home/user/contiki/core/./sys/lc-switch.h :
+ /home/user/contiki/core/./sys/cc.h :
+ /home/user/contiki/core/./sys/autostart.h :
+ /home/user/contiki/core/./sys/process.h :
+ /home/user/contiki/core/./sys/timer.h :
+ /home/user/contiki/core/./sys/clock.h :
+ /home/user/contiki/core/./sys/ctimer.h :
+ /home/user/contiki/core/./sys/etimer.h :
+ /home/user/contiki/core/./sys/timer.h :
+ /home/user/contiki/core/./sys/etimer.h :
+ /home/user/contiki/core/./sys/rtimer.h :
+ /home/user/contiki/cpu/msp430/./rtimer-arch.h :
+ /home/user/contiki/core/./sys/rtimer.h :
+ /home/user/contiki/core/./sys/pt.h :
+ /home/user/contiki/core/./sys/procinit.h :
+ /home/user/contiki/core/./sys/loader.h :
+ /home/user/contiki/core/./sys/clock.h :
+ /home/user/contiki/core/./sys/energest.h :
+ /home/user/contiki/dev/cc2420/cc2420.h :
+ /home/user/contiki/core/./dev/spi.h :
+ /home/user/contiki/core/./dev/radio.h :
+ /home/user/contiki/dev/cc2420/cc2420_const.h :
+ /home/user/contiki/core/./lib/aes-128.h :
+ /home/user/contiki/core/./dev/radio-sensor.h :
